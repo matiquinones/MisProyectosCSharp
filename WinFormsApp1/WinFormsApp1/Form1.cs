6da068557eef19a9eeabe7081bd4d48abd1e3db2
matiquinones <ma.quinones@duocuc.cl>
2025-03-05 15:54:12 -0300

    Primer commit - subiendo proyectos

---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int i = 50;
+            int j = 40;
+
+            int y = i + j;
+            MessageBox.Show(y.ToString());
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
